e0990dccfb4ef450c0cb6cbcf0a91fab4c3185c5
AmritSap <amritsapkota32@gmail.com>
2021-09-28 23:13:10 +1000

    Converted iso date into AEST

--- src/utilities/podcast-processor.js
+++ src/utilities/podcast-processor.js
@@ -1,49 +1,53 @@
 const getRSSFeed = require("./rss-parser");
-  
-const podcastProcessor = async sort =>{
-    try {
-        const rssSource =
-          "https://www.nasa.gov/rss/dyn/Houston-We-Have-a-Podcast.rss";
-          const rss =await getRSSFeed(rssSource);
-          
-        // check if rss has got result back if so then we destructure it and take the title description and items
-        if(rss){
-            const {title,description,items}=rss;
+const { convertISODateToAEST } = require("./format-iso-date-to-AEST");
 
-            // take out ten episodes from item
-            let tenEpisodes =items.slice(0,10);
+const podcastProcessor = async (sort) => {
+  try {
+    const rssSource =
+      "https://www.nasa.gov/rss/dyn/Houston-We-Have-a-Podcast.rss";
+    const rss = await getRSSFeed(rssSource);
 
-            const episodesInfo = tenEpisodes.map((item) => {
-              return {
-                title: item.title,
-                audioUrl: item.enclosure.url,
-                publishedDate: item.pubDate,
-              };
-            });
+    // check if rss has got result back if so then we destructure it and take the title description and items
+    if (rss) {
+      const { title, description, items } = rss;
 
-            let sorted = [];
+      // take out ten episodes from item
+      let tenEpisodes = items.slice(0, 10);
 
-            if (sort === "dsc") {
-              sorted = episodesInfo.sort(
-                (a, b) =>
-                  new Date(b.publishedDate).getTime() -
-                  new Date(a.publishedDate).getTime()
-              );
-            } else if (sort === "asc") {
-              sorted = episodesInfo.sort(
-                (a, b) =>
-                  new Date(a.publishedDate).getTime() -
-                  new Date(b.publishedDate).getTime()
-              );
-            } else {
-              sorted = episodesInfo;
-            }
-            return sorted;
+      const episodesInfo = tenEpisodes.map((item) => {
+        return {
+          title: item.title,
+          audioUrl: item.enclosure.url,
+          publishedDate: item.pubDate,
+        };
+      });
 
-        }
-    } catch (error) {
-        throw new Error(error);
+      let sorted = [];
+
+      if (sort === "dsc") {
+        sorted = episodesInfo.sort(
+          (a, b) =>
+            new Date(b.publishedDate).getTime() -
+            new Date(a.publishedDate).getTime()
+        );
+      } else if (sort === "asc") {
+        sorted = episodesInfo.sort(
+          (a, b) =>
+            new Date(a.publishedDate).getTime() -
+            new Date(b.publishedDate).getTime()
+        );
+      } else {
+        sorted = episodesInfo;
+      }
+      const episodes = sorted.map((item) => {
+        item.publishedDate = convertISODateToAEST(item.publishedDate);
+        return item;
+      });
+      return episodes;
     }
-}
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
 module.exports = { podcastProcessor };
